8cea2f2f9d4c362fa754d33e9adac9c07cb8428c
Glenn Y. Rolland <glenux@glenux.net>
2018-08-21 10:57:59 +0200

    Add doc

--- cmd/trello2mail/import.go
+++ cmd/trello2mail/import.go
@@ -1,10 +1,31 @@
 // create taskell configuration
-// run taskell and read content
+// run taskell and read content ?
+// use https://github.com/adlio/trello ?
 
 package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func runcmd(command string) string {
+	shell := "/bin/sh"
+	flag := "-c"
+
+	out, err := exec.Command(shell, flag, command).Output()
+	if err != nil {
+		panic(err)
+	}
+
+	return string(out)
+}
 
 func CreateTaskell() {
 }
 
-func RunTaskell() {
+func RunTaskell(boardUrl string) {
+	cmd := fmt.Sprintf("taskell -t %s -", boardUrl)
+	markdown := strings.TrimSpace(runcmd(cmd))
 }
